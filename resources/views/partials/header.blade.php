7428b9d36ab298fc0ec6ae73b4e7f50ad498bb7f
JonasDudonis <74532923+JonasDudonis@users.noreply.github.com>
2021-04-18 09:21:56 +0300

    header commit

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,5 @@
 <nav class="navbar navbar-light" style="background-color: #bbe3ff; justify-content: center;">
     <a class="navbar-brand"; style="font-size: 50px"; href="/">Laravel CRUD Project Manager</a>
-    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button> --}}
-    <br>
 </nav>
 <div class="" style="background-color: #bbe3ff; justify-content: center;">
     <a class="btn btn-sm btn-outline-secondary {{ Request::routeIs('employees.index') ? 'active' : '' }}"
